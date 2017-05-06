28461ddb349b2befa338d63b10b57af5e504cdda
Stoyan Ivanov <stoian_ivanov_ivanov@abv.bg>
2017-05-06 14:17:21 +0300

    delete all

--- README.md
+++
@@ -1,2 +0,0 @@
-# HackSofiaBigMoney
-This a start up project for the good wills of the Qko KOD.
--- src/main/java/someId/SomeController.java
+++
@@ -1,8 +0,0 @@
-package someId;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class SomeController {
-
-}
